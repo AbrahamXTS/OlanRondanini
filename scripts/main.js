01045d9170e2698f3267c02d16da9f1f486d24f9
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2021-12-27 16:36:04 -0600

    Secciones añadidas

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 	const menuMovilOpciones = document.querySelectorAll(".navegacion__opciones--movil a");
 
 	botonNavegacion.addEventListener("click", () => {
-		if (menuMovil.style.height == "230.4px") {
+		if (menuMovil.style.height == "185.4px") {
 			menuMovil.style.height = "0rem";
 			menuMovilOpciones.forEach((opcion) => (opcion.style.display = "none"));
 		} else {
-			menuMovil.style.height = "230.4px";
+			menuMovil.style.height = "185.4px";
 			setTimeout(
 				() => menuMovilOpciones.forEach((opcion) => (opcion.style.display = "block")),
 				100
